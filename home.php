322d3c54f2c8602773bec372d1461d2227e42ba0
diimasfebri <dimas.febri21@gmail.com>
2021-06-08 07:27:55 +0700

    dimas

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Home </title>
+	
+</head>
+<body>
+<div class="isi" >test</div>
+
+
+
+</body>
+
+
+
+<style type="text/css">
+	body{
+		position: relative;
+		display: flex;
+		width: 100vw;
+		height: 100vh;
+		background-color: #EEEE;	
+		isi {
+
+		}
+	}
+</style>
+</html>
